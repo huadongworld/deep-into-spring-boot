37777e2bde54a0cc905cc7c50a9c5e66a0d63576
huadong <hd2260332898@gmail.com>
2018-12-01 15:14:34 +0800

    自动装配模块创建

--- autoconfigure/src/main/java/com/ys/autoconfigure/AutoconfigureApplication.java
+++ autoconfigure/src/main/java/com/ys/autoconfigure/AutoconfigureApplication.java
@@ -1,15 +1,16 @@
-package com.ys.springboot;
+package com.ys.autoconfigure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author HuaDong
- * @date 2018/12/1 15:02
+ * @date 2018/12/1 15:11
  */
 @SpringBootApplication
-public class SpringbootApplication {
+public class AutoconfigureApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(AutoconfigureApplication.class, args);
     }
 }
--- autoconfigure/src/test/java/com/ys/autoconfigure/AutoconfigureApplicationTests.java
+++ autoconfigure/src/test/java/com/ys/autoconfigure/AutoconfigureApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ys.springboot;
+package com.ys.autoconfigure;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootApplicationTests {
+public class AutoconfigureApplicationTests {
 
     @Test
     public void contextLoads() {

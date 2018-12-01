d19e0ee163736f7652674ae34ec28798f223c94b
huadong <hd2260332898@gmail.com>
2018-12-01 15:05:27 +0800

    first commit

---
+++ src/main/java/com/ys/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.ys.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuaDong
+ * @date 2018/12/1 15:02
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}

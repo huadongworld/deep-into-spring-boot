1493175a1dc4662859d989aa9740df60ee36376a
huadong <hd2260332898@gmail.com>
2018-12-01 21:01:12 +0800

    @Profile基于配置方式实现自定义条件装配

---
+++ autoconfigure/src/main/java/com/ys/autoconfigure/bootstrapt/CalculateServiceBootstrap.java
@@ -0,0 +1,35 @@
+package com.ys.autoconfigure.bootstrapt;
+
+import com.ys.autoconfigure.service.CalculateService;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author HuaDong
+ * @date 2018/12/1 20:54
+ *
+ * {@link CalculateService} 引导类
+ */
+@SpringBootApplication(scanBasePackages = "com.ys.autoconfigure.service")
+public class CalculateServiceBootstrap {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(CalculateServiceBootstrap.class)
+                .web(WebApplicationType.NONE)
+                //配置方式实现自定义条件装配
+                .profiles("Java8")
+                .run(args);
+
+        // CalculateService Bean 是否存在
+        CalculateService calculateService = context.getBean(CalculateService.class);
+
+        System.out.println("calculateService.sum(1...10) : " +
+                calculateService.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        // 关闭上下文
+        context.close();
+    }
+}

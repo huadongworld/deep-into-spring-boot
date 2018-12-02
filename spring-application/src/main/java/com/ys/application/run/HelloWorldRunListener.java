6f11db4ab17ec07d933f5451d7b21216f34de9d0
huadong <hd2260332898@gmail.com>
2018-12-02 19:13:41 +0800

    SpringApplication运行监听器

---
+++ spring-application/src/main/java/com/ys/application/run/HelloWorldRunListener.java
@@ -0,0 +1,54 @@
+package com.ys.application.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author HuaDong
+ * @date 2018/12/2 19:03
+ *
+ * HelloWorld {@link SpringApplicationRunListener}
+ */
+public class HelloWorldRunListener implements SpringApplicationRunListener {
+
+    public HelloWorldRunListener(SpringApplication application, String[] args) {
+
+    }
+
+    @Override
+    public void starting() {
+        System.out.println("HelloWorldRunListener.starting()...");
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+
+    }
+}

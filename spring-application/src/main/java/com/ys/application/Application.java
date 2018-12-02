faa1d627c855d8574f89c117ae474be92d701a94
huadong <hd2260332898@gmail.com>
2018-12-02 12:01:00 +0800

    理解SpringApplication模块创建

---
+++ spring-application/src/main/java/com/ys/application/Application.java
@@ -0,0 +1,16 @@
+package com.ys.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuaDong
+ * @date 2018/12/2 11:59
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

5b1a179d4ce9596adf54981b13d5ec64ab5645d0
huadong <hd2260332898@gmail.com>
2018-12-05 23:16:55 +0800

    DeferredResult异步执行

--- spring-servlet/src/main/java/com/ys/spring/web/servlet/controller/HelloWorldAsyncController.java
+++ spring-servlet/src/main/java/com/ys/spring/web/servlet/controller/HelloWorldAsyncController.java
@@ -19,12 +19,33 @@ import java.util.concurrent.*;
 @EnableScheduling
 public class HelloWorldAsyncController {
 
+    private final BlockingQueue<DeferredResult<String>> queue = new ArrayBlockingQueue<>(5);
+
+    // 超时随机数
+    private final Random random = new Random();
+
+    @Scheduled(fixedRate = 5000)
+    public void process() throws InterruptedException { // 定时操作
+        DeferredResult<String> result = null;
+        do {
+            result = queue.take();
+            // 随机超时时间
+            long timeout = random.nextInt(100);
+            // 模拟等待时间，RPC 或者 DB 查询
+            Thread.sleep(timeout);
+            // 计算结果
+            result.setResult("Hello,World");
+            println("执行计算结果，消耗：" + timeout + " ms.");
+        } while (result != null);
+    }
+
     @GetMapping("/hello-world")
     public DeferredResult<String> helloWorld() {
 
         //设置超时时间
         DeferredResult<String> result = new DeferredResult<>(50L);
 
+        queue.offer(result);
         println("Hello,World");
 
         //线程结束执行

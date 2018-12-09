fd12bda9fe9bcd934c30ca3ab1cbb0a3080a1e2e
huadong <hd2260332898@gmail.com>
2018-12-09 22:49:42 +0800

    SpringBoot Webflux编程模型之Function Endpoint

---
+++ springboot-webflux/src/main/java/com/ys/web/reactive/bootstrapt/WebFluxApplication.java
@@ -0,0 +1,63 @@
+package com.ys.web.reactive.bootstrapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+/**
+ * @author HuaDong
+ * @date 2018/12/9 21:48
+ *
+ * SpringBoot Webflux编程模型之Function Endpoint，另一种是注解驱动
+ */
+@SpringBootApplication
+@RestController
+public class WebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+
+//        // 写法一：
+//        return RouterFunctions.route(
+//                // 判断请求是否匹配
+//                request -> {
+//                    URI uri = request.uri();
+//                    return "/hello-world".equals(uri.getPath());
+//                },
+//                // 绑定实现
+//                request -> ServerResponse
+//                        .status(HttpStatus.OK)
+//                        .body(Mono.just("Hello World"), String.class)
+//        );
+
+        // 写法二：
+        return route(GET("/hello-world"), this::helloWorld);
+    }
+
+    public Mono<ServerResponse> helloWorld(ServerRequest serverRequest) {
+
+        println("helloWorld");
+        return ServerResponse.status(HttpStatus.OK)
+                .body(Mono.just("Hello,World"), String.class);
+
+    }
+
+    private static void println(String message) {
+        System.out.println("[" + Thread.currentThread().getName() + "] : " + message);
+    }
+
+}
